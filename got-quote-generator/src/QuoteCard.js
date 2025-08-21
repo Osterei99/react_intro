0e0fd1d2924695728b1703a22cd62a83e480d3b4
Kai Brokmann <133008349+Osterei99@users.noreply.github.com>
2025-08-21 09:25:04 +0200

    Button

--- got-quote-generator/src/QuoteCard.js
+++ got-quote-generator/src/QuoteCard.js
@@ -1,15 +1,32 @@
 import React from "react";
 
+  const quoteStyle = {
+    fontStyle: 'italic',
+    color: '#f89a03ff',
+    fontSize: '1.5em',
+    marginBottom: '10px',
+    borderLeft: '4px solid #f89a03ff',
+    paddingLeft: '15px'
+  };
 
-function QuoteCard(props) {
+  const characterStyle = {
+    fontSize: '0.8em',
+    color: '#434343ff',
+    marginTop: '5px',
+    display: 'block'
+  };
+
+
+function QuoteCard({ quoteText, characterName, isQuoteEpic, children }) {
 
     return (
-        <blockquote style={props.quoteStyle}>
-            "{props.quoteText}"
-            <footer style={props.characterStyle}>
-                - {props.characterName}
-                {props.isQuoteEpic && <span style={{ marginLeft: '10px' }}>🌟</span>}
+        <blockquote style={quoteStyle}>
+            "{quoteText}"
+            <footer style={characterStyle}>
+                - {characterName}
+                {isQuoteEpic && <span style={{ marginLeft: '10px' }}>🌟</span>}
             </footer>
+            {children}
         </blockquote>
     )
 }
